39e77fae2ed4b3f0c458b25bdc5ea49b3338ec7d
Davide Brognoli <davide.brognoli@gmail.com>
2019-11-17 15:02:15 +0100

    Gatsby Default Starter

---
+++ src/pages/privacy.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Privacy = () => (
+  <Layout>
+    <SEO title="Privacy and Cookie policy" />
+    <h1>Privacy and Cookie policy</h1>
+  </Layout>
+)
+
+export default Privacy
